a7ae7d5fd4359f8030fc72900d161ea01de579a6
exes <robert.marcelino@outloo.com>
2019-04-02 23:57:55 -0400

    connf. para digital ocean

--- Capfile
+++ Capfile
@@ -5,8 +5,9 @@ require 'capistrano/deploy'
 
 require 'capistrano/rails'
 require 'capistrano/bundler'
-require 'capistrano/rvm'
+require 'capistrano/rbenv'
 require 'capistrano/puma'
+install_plugin Capistrano::Puma  # Default puma tasks
 
 # Loads custom tasks from `lib/capistrano/tasks' if you have any defined.
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server "157.230.230.88", port: your_port_num, roles: [:web, :app, :db], primary: true
+server "157.230.230.88", port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:exes21/proyecto_grado_web.git'
 set :application,     'IARW'
@@ -22,6 +22,17 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+# config/deploy.rb
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.5.3'
+
+# in case you want to set ruby version from the file:
+# set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
 ## Defaults:
 # set :scm,           :git
