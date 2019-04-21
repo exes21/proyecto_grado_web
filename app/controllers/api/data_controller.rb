0042580b26fb2936efe8be4f27393aff0345d398
= <=>
2019-04-21 12:27:59 -0400

    fix issue with bssid duplicate

--- app/controllers/api/data_controller.rb
+++ app/controllers/api/data_controller.rb
@@ -136,9 +136,9 @@ class Api::DataController < ApplicationController
   end
 
   def set_access_point
-    @ap = ApChannel.find_by(bssid: params['MacDelRouter']) ||
-          AccessPoint.find_by(mac_address: params['MacDelRouter']) ||
+    @ap = AccessPoint.find_by(mac_address: params['MacDelRouter']) ||
           AccessPoint.find_by(ip_address: params["DefaultGate"].split('.').reverse.join('.')) ||
+          ApChannel.find_by(bssid: params['MacDelRouter']) ||
           AccessPoint.find_by(ssid: JSON.parse(params['ssid']))
     #ip_address: params["DefaultGate"].split('.').reverse.join('.')
 
